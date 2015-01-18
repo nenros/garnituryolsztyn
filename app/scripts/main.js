ccebe09295a6659ccbb73e92ab2ef7cb5b0d6c29
Bartosz Markowski <nenros@gmail.com>
2015-01-18 22:20:08 +0100

    First published version.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var itemsToShow = 0;
+var sliderInterval;
 
 var addEvent = function(el, eventType, handler) {
   if (el.addEventListener) { // DOM Level 2 browsers
@@ -43,7 +44,7 @@ var nextImage = function(){
   var slider = document.getElementById('slides');
   var slides = slider.getElementsByTagName('li');
   var el = slides[0];
-  slides[0].classList.remove('active');
+  el.classList.remove('active');
   el.parentNode.appendChild(el);
   slides[itemsToShow - 1].classList.add('active');
 };
@@ -62,24 +63,24 @@ var sliderItems = function(){
   var slides = slider.getElementsByTagName('li');
   var width = slider.offsetWidth;
  if (document.body.clientWidth  <= 480) {
-   slider.forEach(function(slide){
+   slides.forEach(function(slide){
      slide.classList.remove('active');
    });
    return;
  }
   var newItemsToShow = Math.round(width / 300);
   if (newItemsToShow !== itemsToShow){
-    clearInterval(sliderInterval);
     slider.classList.remove('item-'+itemsToShow);
-    itemsToShow = newItemsToShow;
-    slider.classList.add('item-'+itemsToShow);
-    for(var i =0; i < itemsToShow; i++){
+    slider.classList.add('item-'+newItemsToShow);
+    for(var i =0; i < slides.length; i++){
+      if(i < newItemsToShow) {
       slides[i].classList.add('active');
+    } else {
+      slides[i].classList.remove('active');
     }
-    for(var k = itemsToShow; k< slides.length; i++){
-      slides[k].classList.remove('active');
     }
-    sliderInterval = setInterval(nextImage, 3000);
+
+    itemsToShow = newItemsToShow;
   }
 };
 
@@ -96,8 +97,6 @@ addEvent(document, 'DOMContentLoaded', addMenuListeners);
 addEvent(document, 'DOMContentLoaded', sliderItems);
 addEvent(nextImageButton, 'click', nextImage);
 addEvent(prevImageButton, 'click', prevImage);
-
-var sliderInterval = setInterval(nextImage, 3000);
 
 addEvent(slider, 'mouseover', function(){
   clearInterval(sliderInterval);
